739979ffcdd6a582e71236b20eb1427d37d8dbd2
Cyrill <cyrillkuettel@gmail.com>
2022-05-15 16:02:17 +0200

    add working configuration, print bluetooth Device Adress

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,71 @@
-#include <Arduino.h>
 #include "BluetoothSerial.h"
+#include <WiFi.h>
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+// Check if Bluetooth configs are enabled
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
 // Bluetooth Serial object
 BluetoothSerial SerialBT;
+
+// GPIO where LED is connected to
+const int ledPin =  25;
+
+      
+
 
 // Handle received and sent messages
 String message = "";
 char incomingChar;
 
-void setup() {
-  Serial.begin(115200);
-  // Bluetooth device name
-  SerialBT.begin("ESP32");
-  Serial.println("The device started, now you can pair it with bluetooth!");
-  Serial.println();
-
-
+void printDeviceAddress() {
+ 
+  const uint8_t* point = esp_bt_dev_get_address();
+ 
+  for (int i = 0; i < 6; i++) {
+ 
+    char str[3];
+ 
+    sprintf(str, "%02X", (int)point[i]);
+    Serial.print(str);
+ 
+    if (i < 5){
+      Serial.print(":");
+    }
+ 
+  }
+  Serial.print("\n");
 }
 
-void loop() {
-  // Serial.print("ESP Board MAC Address:  ");
-  // Serial.println(WiFi.macAddress());
+
+
+void setup() {
+  Serial.begin(115200);
   
-  // Read received messages (LED control command)
+  pinMode(ledPin, OUTPUT);
+  
+
+  SerialBT.begin("ESP32"); // Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  printDeviceAddress();
+
+  if(!SerialBT.begin("ESP32")){
+    Serial.println("An error occurred initializing Bluetooth");
+  }else{
+    Serial.println("Bluetooth initialized");
+  }
+
+  SerialBT.write(1); // Starting signal to phone
+}
+
+
+boolean startSignalSent = false;
+
+void loop() {
   if (SerialBT.available()){
     char incomingChar = SerialBT.read();
-
     if (incomingChar != '\n'){
       message += String(incomingChar);
     }
@@ -33,6 +74,13 @@ void loop() {
     }
     Serial.write(incomingChar);  
   }
-  
+
   delay(20);
+
+  if (SerialBT.connected()) {
+      delay(5000);
+      SerialBT.write(1);  
+      startSignalSent = true;
+  }
+
 }
